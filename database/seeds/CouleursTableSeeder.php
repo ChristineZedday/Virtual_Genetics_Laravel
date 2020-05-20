dc657696ce4cccada111550ba0253bef626bc601
ChristineZedday <christine_bache@yahoo.fr>
2020-05-20 20:53:26 +0200

    tosa

--- database/seeds/CouleursTableSeeder.php
+++ database/seeds/CouleursTableSeeder.php
@@ -241,7 +241,7 @@ class CouleursTableSeeder extends Seeder
             array (
                 'id' => 21,
                 'nom' => 'tobianosabino',
-                'image_id' => NULL,
+                'image_id' => 16,
                 'base_couleur' => 0,
                 'est_motif' => 1,
                 'est_dilution' => 0,
