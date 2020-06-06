e4d2ae10374b15d5909f741ac2bb285345668b4a
ChristineZedday <christine_bache@yahoo.fr>
2020-06-06 13:02:18 +0200

    hh

--- resources/views/animal.blade.php
+++ resources/views/animal.blade.php
@@ -45,24 +45,34 @@
 		<div class='pedigree'>
 				@if (! $animal->fondateur)
 						<span class='generation'>
-							<span class='male'>{{$animal->Sire->NomComplet()}}</span>
-							<span class='femelle'>{{$animal->Dam->NomComplet()}}</span>
+							<span class='male'>
+								<a href="{{route('animal',[$elevage->id, $animal->Sire->id])}}"> 
+									{{$animal->Sire->NomComplet()}}</a>
+							 {{$animal->Sire->Race->nom}}</span>
+							
+							<span class='femelle'><a href="{{route('animal',[$elevage->id, $animal->Dam->id])}}">
+								 {{$animal->Dam->NomComplet()}}</a>
+								{{$animal->Dam->Race->nom}}</span>
 						</span>
 						<span class='generation'>
-							<span class='male'>@isset($animal->Sire->Sire) {{$animal->Sire->Sire->NomComplet()}} @else gpp @endisset </span>
-							<span class='femelle'>@isset($animal->Sire->Dam) {{$animal->Sire->Dam->NomComplet()}} @else gmp @endisset </span>
-							<span class='male'>@isset($animal->Dam->Sire) {{$animal->Dam->Sire->NomComplet()}} @else gpm @endisset </span>
-							<span class='femelle'> @isset($animal->Dam->Dam) {{$animal->Dam->Dam->NomComplet()}} @else gmm @endisset </span>
+							<span class='male'>@isset($animal->Sire->Sire)<a href="{{route('animal',[$elevage->id, $animal->Sire->Sire->id])}}">  {{$animal->Sire->Sire->NomComplet()}} </a>
+							{{$animal->Sire->Sire->Race->nom}}@else gpp @endisset </span>
+							<span class='femelle'>@isset($animal->Sire->Dam)<a href="{{route('animal',[$elevage->id, $animal->Sire->Dam->id])}}">  {{$animal->Sire->Dam->NomComplet()}} </a>{{$animal->Sire->Dam->Race->nom}}@else gmp @endisset </span>
+							<span class='male'>@isset($animal->Dam->Sire) <a href="{{route('animal',[$elevage->id, $animal->Dam->Sire->id])}}">  {{$animal->Dam->Sire->NomComplet()}} </a>
+							{{$animal->Dam->Sire->Race->nom}}@else gpm @endisset </span>
+							<span class='femelle'> @isset($animal->Dam->Dam) <a href="{{route('animal',[$elevage->id, $animal->Dam->Dam->id])}}"> {{$animal->Dam->Dam->NomComplet()}} </a>{{$animal->Dam->Dam->Race->nom}} @else gmm @endisset </span>
 						</span>
 						<span class='generation'>
-							<span class='male'>@isset($animal->Sire->Sire->Sire) {{$animal->Sire->Sire->Sire->NomComplet()}} @else agpp @endisset </span>
-							<span class='femelle'>@isset($animal->Sire->Sire->Dam) {{$animal->Sire->Sire->Dam->NomComplet()}} @else agmp @endisset </span>
-							<span class='male'>	@isset($animal->Sire->Dam->Sire) {{$animal->Sire->Dam->Sire->NomComplet()}} @else agpp @endisset </span>	
-							<span class='femelle'>@isset($animal->Sire->Dam->Dam) {{$animal->Sire->Dam->Dam->NomComplet()}} @else agmp @endisset </span>
-							<span class='male'>	@isset($animal->Dam->Sire->Sire) {{$animal->Dam->Sire->Sire->NomComplet()}} @else agpm @endisset </span>
-							<span class='femelle'>@isset($animal->Dam->Sire->Dam) {{$animal->Dam->Sire->Dam->NomComplet()}} @else agmm @endisset </span>
-							<span class='male'>@isset($animal->Dam->Dam->Sire) {{$animal->Dam->Dam->Sire->NomComplet()}} @else agmp @endisset </span>
-							<span class='femelle'>@isset($animal->Dam->Dam->Dam) {{$animal->Dam->Dam->Dam->NomComplet()}} @else agmm @endisset </span>
+							<span class='male'>@isset($animal->Sire->Sire->Sire) <a href="{{route('animal',[$elevage->id, $animal->Sire->Sire->Sire->id])}}"> {{$animal->Sire->Sire->Sire->NomComplet()}} </a> @else agpp @endisset </span>
+							<span class='femelle'>@isset($animal->Sire->Sire->Dam)  <a href="{{route('animal',[$elevage->id, $animal->Sire->Sire->Dam->id])}}">  {{$animal->Sire->Sire->Dam->NomComplet()}} </a> @else agmp @endisset </span>
+							<span class='male'>	@isset($animal->Sire->Dam->Sire)  <a href="{{route('animal',[$elevage->id, $animal->Sire->Dam->Sire->id])}}">  {{$animal->Sire->Dam->Sire->NomComplet()}} </a> @else agpp @endisset </span>	
+							<span class='femelle'>@isset($animal->Sire->Dam->Dam) 
+							<a href="{{route('animal',[$elevage->id, $animal->Sire->Dam->Dam->id])}}"> {{$animal->Sire->Dam->Dam->NomComplet()}}  </a> @else agmp @endisset </span>
+							<span class='male'>	@isset($animal->Dam->Sire->Sire)  <a href="{{route('animal',[$elevage->id, $animal->Dam->Sire->Sire->id])}}"> {{$animal->Dam->Sire->Sire->NomComplet()}} </a>  @else agpm @endisset </span>
+							<span class='femelle'>@isset($animal->Dam->Sire->Dam)  <a href="{{route('animal',[$elevage->id, $animal->Dam->Sire->Dam->id])}}"> {{$animal->Dam->Sire->Dam->NomComplet()}} </a> @else agmm @endisset </span>
+							<span class='male'>@isset($animal->Dam->Dam->Sire)
+							<a href="{{route('animal',[$elevage->id, $animal->Dam->Dam->Sire->id])}}">  {{$animal->Dam->Dam->Sire->NomComplet()}} </a> @else agmp @endisset </span>
+							<span class='femelle'>@isset($animal->Dam->Dam->Dam) <a href="{{route('animal',[$elevage->id, $animal->Dam->Dam->Dam->id])}}">  {{$animal->Dam->Dam->Dam->NomComplet()}} </a>  @else agmm @endisset </span>
 						</span>	
 						
 				@endif
@@ -118,7 +128,7 @@
 	
 			<div id="progeny">
 				@foreach ($animal->Progeny() as $petit)
-				<p>{{$petit->NomComplet()}} @if ($animal->sexe == 'femelle' || $animal->sexe == 'vieille femelle') par {{$petit->Sire->NomComplet()}} @endif</p>
+				<p> <a href="{{route('animal',[$elevage->id, $petit->id])}}"> {{$petit->NomComplet()}} </a> @if ($animal->sexe == 'femelle' || $animal->sexe == 'vieille femelle') par {{$petit->Sire->NomComplet()}} @endif</p>
 				@endforeach
 			</div>
 		</div>
