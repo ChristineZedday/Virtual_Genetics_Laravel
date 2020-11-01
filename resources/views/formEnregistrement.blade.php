140d8a9cc032734ec4d8d259ecd267720f6756ed
Christine Zedday <christine_bache@yahoo.fr>
2020-11-01 19:31:22 +0100

    photo sur form enreg

--- resources/views/formEnregistrement.blade.php
+++ resources/views/formEnregistrement.blade.php
@@ -1,60 +1,65 @@
 @extends('layouts.elevageDashboard')
 
 @section('content')
-<div>
-<form action="{{route('registration',[$animal->id])}}" method="POST">
-		@method('PUT')
-        @csrf
-	<h2 class='form'>Enregistrer votre produit</h2>
-	@if(Session::has('status'))
-<p class="{{ Session::get('alert-class')}}" >{{ Session::get('status') }}</p>
-@endif
-	<div class='form'>
-	<label for="nom">Entrez le nom que vous avez choisi pour votre animal (sans l'affixe si vous en avez un)</label><br/>
-	<input type="text" name="nom" value="@isset($animal->nom) {{$animal->nom}}  @endisset"/>
+<div id="content">
+	<div id="enr">
+		<form action="{{route('registration',[$animal->id])}}" method="POST">
+			@method('PUT')
+			@csrf
+			<h2 class='form'>Enregistrer votre produit</h2>
+			@if(Session::has('status'))
+			<p class="{{ Session::get('alert-class')}}" >{{ Session::get('status') }}</p>
+			@endif
+			<div class='form'>
+			<label for="nom">Entrez le nom que vous avez choisi pour votre animal (sans l'affixe si vous en avez un)</label><br/>
+			<input type="text" name="nom" value="@isset($animal->nom) {{$animal->nom}}  @endisset"/>
+			</div>
+			<div class='form'>
+			<label for="couleur">Entrez la couleur de votre animal.</label><br/>
+			<input type="text" name="couleur" value="@isset($animal->couleur) {{$animal->couleur}}  @endisset"/>
+			</div>
+			
+			
+			<div class='form'>
+			<p>Les animaux de pure race sont automatiquement enregistrés dans le livre généalogique de la race sans frais supplémentaires, sous réserve que le père soit autorisé ou approuvé. Les animaux issus de croisement ou d'étalons non autorisés sont enregistrés comme Origine Constatée (OC), gratuitement, mais vous avez la possibité sous certaines conditions de les faire enregistrer dans un stud book ouvert (acceptant des animaux dont les deux parents ne sont pas inscrits), moyennant paiement d'un droit d'inscription.
+			Dans ce cas veuillez trouver la liste ci-dessous. Si l'animal est inscriptible dans plusieurs registres, un seul peut-être choisi, conformémént au règlement des Haras Nationaux.
+			@isset($races)</p>
+			<input type="hidden" id ="budget" value ="{{$elevage->budget}}"/>
+			<label for="race">Vous avez la possibilité d'enregistrer votre produit, moyennant des frais variables, dans l'une des races suivantes:</label><br/>
+			<select  name="race">
+				<option value="1">OC 0</option>
+					@foreach ($races as $race)
+					<option value="{{$race->id}}">{{$race->nom}} {{$race->frais_enregistrement}}</option>	
+					@endforeach
+			</select> 
+
+			@endisset
+			</div>
+			<div class='form'> 
+				<button onclick="checkBudget">Valider</button>
+				</div>
+		</form>
 	</div>
-	<div class='form'>
-	<label for="couleur">Entrez la couleur de votre animal.</label><br/>
-	<input type="text" name="couleur" value="@isset($animal->couleur) {{$animal->couleur}}  @endisset"/>
-	</div>
-	
-	<div id='imb'>					
-		<div id='image'>
+
+
+					
+	<div id='image'>	
 			<figure class='jeu'>
 			<?php
 			$images = $animal->Image;
 			if (isset($images)) {
 				$path0 ="'/img/fond.png'";
-				echo '<img src='.$path0. ' style="z-index:0; position:absolute; width:600px; height:450px;" >';
+				echo '<img src='.$path0. ' style="z-index:0; position:absolute; width:400px; height:300px;" >';
 					foreach ($images as $image) {
 						$path = "'/img/".$image->chemin.".png'";
-						$style = "position:absolute; width:600px; height:450px; z-index:".$image->z_index; 
+						$style = "position:absolute; width:400px; height:300px; z-index:".$image->z_index; 
 						echo"<img src=".$path. " style ='" . $style."'>";
 					}
 			}
 			?>
 			</figure>
-		</div>
-		<div class='form'>
-	<p>Les animaux de pure race sont automatiquement enregistrés dans le livre généalogique de la race sans frais supplémentaires, sous réserve que le père soit autorisé ou approuvé. Les animaux issus de croisement ou d'étalons non autorisés sont enregistrés comme Origine Constatée (OC), gratuitement, mais vous avez la possibité sous certaines conditions de les faire enregistrer dans un stud book ouvert (acceptant des animaux dont les deux parents ne sont pas inscrits), moyennant paiement d'un droit d'inscription.
-	Dans ce cas veuillez trouver la liste ci-dessous. Si l'animal est inscriptible dans plusieurs registres, un seul peut-être choisi, conformémént au règlement des Haras Nationaux.
-	@isset($races)</p>
-	<input type="hidden" id ="budget" value ="{{$elevage->budget}}"/>
-	<label for="race">Vous avez la possibilité d'enregistrer votre produit, moyennant des frais variables, dans l'une des races suivantes:</label><br/>
-	<select  name="race">
-	<option value="1">OC 0</option>
-		@foreach ($races as $race)
-		<option value="{{$race->id}}">{{$race->nom}} {{$race->frais_enregistrement}}</option>	
-		@endforeach
-		
-</div>
- 
-</select> 
-	@endisset
-	<div class='form'> 
-		<button onclick="checkBudget">Valider</button>
-		</div>
-	</form>
+	</div>
+
 </div>
 <script>
 function checkBudget() 
