2cc8752c86f289e91e6a20c147a39c89d3b2a0d8
Christine Zedday <christine_bache@yahoo.fr>
2020-11-17 19:00:41 +0100

    form val

--- resources/views/formEnregistrement.blade.php
+++ resources/views/formEnregistrement.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="content">
 	<div id="enr">
-		<form action="{{route('registration',[$animal->id])}}" method="POST" onSubmit="return checkBudget()">
+		<form action="{{route('registration',[$animal->id])}}" method="POST" onSubmit="return checkForm()">
 			@method('PUT')
 			@csrf
 			<h2 class='form'>Enregistrer votre produit</h2>
@@ -12,11 +12,11 @@
 			@endif
 			<div class='form'>
 			<label for="nom">Entrez le nom que vous avez choisi pour votre animal (sans l'affixe si vous en avez un)</label><br/>
-			<input type="text" name="nom" value="@isset($animal->nom) {{$animal->nom}}  @endisset"/>
+			<input type="text" name="nom" id="nom" value="@isset($animal->nom) {{$animal->nom}}  @endisset"/>
 			</div>
 			<div class='form'>
 			<label for="couleur">Entrez la couleur de votre animal.</label><br/>
-			<input type="text" name="couleur" value="@isset($animal->couleur) {{$animal->couleur}}  @endisset"/>
+			<input type="text" name="couleur" id="couleur" value="@isset($animal->couleur) {{$animal->couleur}}  @endisset"/>
 			</div>
 			
 			
@@ -63,27 +63,36 @@
 
 </div>
 <script>
-function checkBudget() 
+function checkForm() 
 	{
+		valide = true;
 		budget = document.getElementById('budget').value;	
+		
 		select = document.getElementById('race');
-		
-		
 		opt = select.options[select.selectedIndex].text;
 		//split extraire frais
 		tab = opt.split(" ");
 	
-		frais =tab[tab.length-1];
-		
+		frais =number(tab[tab.length-1]);
 		if (frais > budget)
 		{
 			alert("vous devrez attendre d'être en fonds pour procéder à l'enregistrement");
-			return false;
+			valide = false;
 		}
-		else
+		
+		nom = document.getElementById('nom').value;
+		if (nom == "") 
 		{
-			return true;
+			alert ("saissisez un nom pour cet animal, SVP.");
+			valide = false;
 		}
+		couleur = document.getElementById('couleur').value;
+		if (couleur == "") 
+		{
+			alert ("saissisez la couleur de votre animal, SVP (c'est pas bien grave si vous vous trompez, ça arrive tout le temps).");
+			valide = false;
+		}
+		return valide;
 	}
 	</script>
 
