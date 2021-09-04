8acc5c1fd42a466dac8ce335f20571a65459cbc9
Christine Zedday <christine_bache@yahoo.fr>
2021-09-04 23:13:02 +0200

    tests

--- tests/Feature/AnimalTest.php
+++ tests/Feature/AnimalTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Animal;
 use App\Gamedata;
+use App\Affixe;
 
 class AnimalTest extends TestCase
 {
@@ -18,11 +19,23 @@ class AnimalTest extends TestCase
      *
      * @return void
      */
+
+     static function createGameData() 
+     {
+        $gamedata = New GameData;
+        $gamedata->date_debut = strtotime("1 may 1970");
+        $gamedata->date_courante = strtotime("10 may 1970");
+        $gamedata->budget = 20000;
+        $gamedata->save();
+     }
+
     public function testTailleAdulte()
     {
+        AnimalTest::createGameData();
         $animal = New Animal;
         $animal->taille_cm = 140;
         $animal->date_naissance = strtotime("10 may 1965");
+        dd($animal->ageMonths());
         
 
         $this->assertEquals(140, $animal->taille());
@@ -30,10 +43,11 @@ class AnimalTest extends TestCase
     }
     public function testTailleNewborn()
     {
+        AnimalTest::createGameData();
         $animal = New Animal;
         $animal->taille_cm = 140;
-        $animal->date_naissance = Gamedata::date();
-       
+        $animal->date_naissance = strtotime("10 may 1970");
+        
 
         $this->assertEquals(98, $animal->taille());
 
@@ -41,9 +55,13 @@ class AnimalTest extends TestCase
 
     public function testNomComplet()
     {
+        $affixe = new Affixe;
+        $affixe->libelle ="de Chez Moi";
+        $affixe->affixe_pre = false;
+        $affixe->save();
         $animal = new Animal;
         $animal->nom = "Toto";
         $animal->affixe_id = 1;
-        $this->assertEquals("Toto of Marshwood", $animal->NomComplet());
+        $this->assertEquals("Toto de Chez Moi", $animal->NomComplet());
     }
 }
