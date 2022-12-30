5db94ab4ed4e3d3fe85ffc4a7cb0d8b522f8120a
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2022-12-30 17:22:30 +0100

    commentaire

--- app/Competition.php
+++ app/Competition.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Competition extends Model
 {
-   /* Une compétition est un type d'évènement sportif par exemple concours d'élevage, saut d'obstacles, rassemblant une ou plusieurs épreuves (ex combiné CSO Dressage), pour plusieurs catégories, concerant une ou plusieurs races ou ouvert à toutes, et relevant d'un niveau donné (local, régional, national...).
+   /* Une compétition est un type d'évènement sportif par exemple concours d'élevage, saut d'obstacles, rassemblant une ou plusieurs épreuves (ex combiné CSO Dressage) et relevant d'un niveau donné (local, régional, national...).
    Un évènement est l'instanciation de cette compétition à une date précise.
+
+Les catégories concernent le sexe, la race et l'âge administratif du cheval d'une part, et le tpe d'épreuve, d'autre part.
+
 Dans cette version, un seul type d'épreuve, le concours de Modèle et Allures.
-Les catégories concernent le sexe, la race et l'âge administratif du cheval.
     */
   
 
