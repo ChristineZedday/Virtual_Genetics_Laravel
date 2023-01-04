74530ad32f55173308a64ef33d728ae0179a7577
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-01-04 13:56:11 +0100

    bienvenue

--- resources/views/bienvenue.blade.php
+++ resources/views/bienvenue.blade.php
@@ -5,8 +5,9 @@
 		<p>Bienvenue sur mon application simulateur d'élevage de chevaux, basée sur la génétique</p>
 		<p>Pour jouer, vous devez d'abord vous enregistrer, puis créer au moins un élevage, un budget vous sera alloué pour acheter des reproducteurs et effectuer des actes vétérinaires. Vous pourrez aussi vendre des chevaux.</p>
 		<p>Vous pouvez créer plusieurs élevages si vous le souhaitez</p>
-		<p>Ceci est la version 1.0 qui ne prend pas en compte le coût d'entretien des chevaux (profitez-en bien, petits veinards!) mais qui ne propose pas non plus de compétitions.</p>
+		<p>Ceci est la version 2.0 qui ne prend pas en compte le coût de la nourriture des chevaux (vous êtes censés avoir des hectares et des hectares de bonnes prairies et la compétence pour faire du foin) mais on vous demande quand même de vous acquitter de 10 euros par mois et par cheval pour les frais vétérinaires courants et la maréchalerie.</p>
 		<p>Vos chevaux et poneys naissent, grandissent et vieillissent en fonction du temps: c'est vous qui choisissez votre rythme, en cliquant sur "avancer d'un mois". Attention: Si vous avez plusieurs élevages, tous seront concernés! Ne changez de mois que lorsque vous avez fait tout ce que vous aviez à faire dans tous vos élevages, et entendez-vous avec les autres joueurs si vous êtes plusieurs!</p>
+		<p>La compétition d'élevage (concours de beauté) arrive bientôt!</p>
 	
 		<p>Pour en savoir plus sur la génétique des chevaux, allez sur <a href="http://chrizedday.free.fr">mon site</a>, entre autres!</p>
 	
