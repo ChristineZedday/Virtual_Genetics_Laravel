9eab73a19fa687e6d2bd2a70040ab641a55f915d
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-01-06 11:52:22 +0100

    bi-annuel

--- database/seeds/CompetitionEvenementTableSeeder.php
+++ database/seeds/CompetitionEvenementTableSeeder.php
@@ -121,6 +121,110 @@ class CompetitionEvenementTableSeeder extends Seeder
                 'competition_id' => 5,
                 'evenement_id' => 7,
             ),
+            13 => 
+            array (
+                'id' => 14,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 1,
+                'evenement_id' => 9,
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 8,
+                'evenement_id' => 9,
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 9,
+                'evenement_id' => 9,
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 10,
+                'evenement_id' => 9,
+            ),
+            17 => 
+            array (
+                'id' => 18,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 11,
+                'evenement_id' => 9,
+            ),
+            18 => 
+            array (
+                'id' => 19,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 2,
+                'evenement_id' => 10,
+            ),
+            19 => 
+            array (
+                'id' => 20,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 3,
+                'evenement_id' => 11,
+            ),
+            20 => 
+            array (
+                'id' => 21,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 4,
+                'evenement_id' => 12,
+            ),
+            21 => 
+            array (
+                'id' => 22,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 5,
+                'evenement_id' => 13,
+            ),
+            22 => 
+            array (
+                'id' => 23,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 7,
+                'evenement_id' => 14,
+            ),
+            23 => 
+            array (
+                'id' => 24,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 12,
+                'evenement_id' => 14,
+            ),
+            24 => 
+            array (
+                'id' => 25,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 6,
+                'evenement_id' => 15,
+            ),
+            25 => 
+            array (
+                'id' => 26,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 13,
+                'evenement_id' => 12,
+            ),
         ));
         
         
--- database/seeds/EvenementsTableSeeder.php
+++ database/seeds/EvenementsTableSeeder.php
@@ -23,7 +23,6 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-04-28',
-                
             ),
             1 => 
             array (
@@ -31,7 +30,6 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-04-14',
-                
             ),
             2 => 
             array (
@@ -39,7 +37,6 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-05-02',
-                
             ),
             3 => 
             array (
@@ -47,7 +44,6 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-05-02',
-               
             ),
             4 => 
             array (
@@ -55,7 +51,6 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-06-02',
-               
             ),
             5 => 
             array (
@@ -63,23 +58,76 @@ class EvenementsTableSeeder extends Seeder
                 'created_at' => NULL,
                 'updated_at' => NULL,
                 'date' => '1970-06-07',
-               
             ),
             6 => 
             array (
                 'id' => 7,
                 'created_at' => NULL,
                 'updated_at' => NULL,
-                'date' => '1970-07-07',
-               
+                'date' => '1970-05-07',
             ),
             7 => 
             array (
                 'id' => 8,
                 'created_at' => NULL,
                 'updated_at' => NULL,
-                'date' => '1970-07-02',
-               
+                'date' => '1970-06-02',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-09-06',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-10-12',
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-09-22',
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-09-17',
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-11-14',
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-10-23',
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-10-30',
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'date' => '1970-09-16',
             ),
         ));
         
