db9120cd40d82144e3d83524fe3cf4186c98e159
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-01-27 11:20:36 +0100

    aff niv

--- resources/views/inscrits.blade.php
+++ resources/views/inscrits.blade.php
@@ -8,6 +8,7 @@
 	<th>Nom de l'animal</th>
     <th>Evènement</th>
     <th>Compétition</th>
+    <th>Niveau</th>
     <th>Catégorie</th>
     <th>Actions</th>
 </tr>
@@ -16,6 +17,7 @@
 	<td><a href="{{route('animal', ['elevage'=>$inscrit->Animal->elevage_id, 'animal'=>$inscrit->Animal->id])}}">{{$inscrit->Animal->NomComplet()}}</a></td>
 	<td>{{$inscrit->Evenement->nom}}</td>
     <td>{{$inscrit->Competition->nom}}</td>
+    <td>{{$inscrit->Competition->Niveau->libelle}}</td>
     <td>{{$inscrit->Categorie->nom}} </td>
    
     <td><a href="{{route('desinscrire',['resultat'=>$inscrit])}}">désinscrire</a></td>
