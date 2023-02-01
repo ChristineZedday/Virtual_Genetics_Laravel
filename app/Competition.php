004acf0b61c0cfd376e4254d6bf60cded600dd82
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-02-01 21:29:54 +0100

    oups

--- app/Competition.php
+++ app/Competition.php
@@ -75,7 +75,7 @@ Au moment de l'inscription, les animaux sont inscrits dans un évènement, pour 
         $hasOC = $this->Races->find(1);
         
         if ($hasRace || $hasOC ) {
-           if ($animal->Niveau->id == $this->Niveau->id || $this->Niveau->open)
+           if ($animal->Performance->Niveau->id == $this->Niveau->id || $this->Niveau->open)
             {return true;}
             else {return false;}
         }
