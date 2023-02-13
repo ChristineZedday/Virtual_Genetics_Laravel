edd10da562c0e887d6b4929fa018e3e34a80d4d1
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-02-13 16:08:25 +0100

    foins

--- app/Elevage.php
+++ app/Elevage.php
@@ -7,6 +7,11 @@ use Orangehill\IseedServiceProvider\elevages;
 
 class Elevage extends Model
 {
+    
+    const COEFF_UGB = 0.37;
+    const COEFF_UGP_PLEINE_SUITEE = 0.63 ;//0.37/0.59
+    const PRIX_FOIN = 100;
+
     protected $fillable = ['nom_elevage','nom_eleveur','affixe_id','affixe_pre'];
 
     public function User()
@@ -45,4 +50,66 @@ class Elevage extends Model
         return $res;
 
     }
+
+    private function chargeHa() 
+    //surface prairie nécessaire pour le cheptel
+    {
+        $date = Gamedata::date();
+        $mois = date('m',strtotime($date));
+        $charge = Rendement::where('mois',$mois)->first()->ha_par_UGB;
+        return $charge; 
+      
+    }
+
+    private function faitFoin($surface) {
+        $rendement = Rendement::where('mois',$mois)->first()->foin_tMS_ha;
+        $this->foin = $rendement * $surface;
+
+    }
+
+    private function consommeFoin($UGB) {
+        return $UGB * 4.75/12;
+    }
+
+    public function calculeFrais() 
+{
+    $fraisNourriture = 0;
+    
+    $animaux = Animal::where('$elevage_id',$this->id)->where('foetus',0)->whereNotIn('sexe',['jeune poulain','jeune pouliche'] )->get();
+    $UGB_totaux =0;
+    $fraisVeto =0;
+
+    foreach ($animaux as $animal) {
+        if ($animal->Statut && $animal->Statut->suitee or $animal->Statut->pleine) {
+            $UGB = (self::COEFF_UGB_PLEINE_SUITEE * $animal->taille())/100;
+            $fraisVeto += 25;
+            
+        }
+        else {
+            $UGB = (self::COEFF_UGB * $animal->taille())/100;
+           $fraisVeto += 10;
+        }
+        $UGB_totaux += $UGB;
+    }
+    $charge = chargeHa();
+    $utilise = $UGB_totaux * $charge;
+    if ($utilise >= $this->surface) {
+        faitFoin($this->surface - $utilise);
+    }
+    else {
+        $UGBtrop = $UGB_totaux - (surface * $charge)/$UGB_totaux;
+        $conso = consommeFoin($UGBtrop);
+        if ($conso > $this->foin) {
+            $fraisNourriture = ($this->foin - $conso) * self::PRIX_FOIN;
+            $this->foin = 0;
+        }
+        else {
+            $this->foin -= $conso;
+        }
+        $this->save();
+
+    }
+    return $fraisVeto + $fraisNourriture;
+}
+
 }
