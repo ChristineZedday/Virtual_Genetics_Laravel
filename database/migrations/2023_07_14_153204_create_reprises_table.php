b5f2e9cb44f0e54c79d90bf9535a51cf69f190f5
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-07-14 20:49:18 +0200

    fg

--- database/migrations/2023_07_14_153204_create_reprises_table.php
+++ database/migrations/2023_07_14_153204_create_reprises_table.php
@@ -17,12 +17,12 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->String('nom');
-            $table->enum('categorie', ['poney', 'As Poney','amateur', 'pro', 'international']);
+            $table->enum('categorie', ['club','poney', 'As Poney','amateur', 'pro', 'international']);
             $table->enum('niveau',['4','3','2','1','elite']);
             $table->tinyInteger('taille_min_cm')->unsigned()->default(108);
             $table->tinyInteger('taille_max_cm')->unsigned()->default(149);
             $table->tinyInteger('age_min_administratif')->unsigned()->default(4);
-            $table->tinyInteger('niveau_num_global')->unsigned():
+            $table->tinyInteger('niveau_num_global')->unsigned();
         });
     }
 
