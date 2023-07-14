9d5cb9156480179935a3863bd377ae121ca31a7e
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-07-14 20:46:24 +0200

    dressage

---
+++ database/migrations/2023_07_14_153204_create_reprises_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reprises', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->String('nom');
+            $table->enum('categorie', ['poney', 'As Poney','amateur', 'pro', 'international']);
+            $table->enum('niveau',['4','3','2','1','elite']);
+            $table->tinyInteger('taille_min_cm')->unsigned()->default(108);
+            $table->tinyInteger('taille_max_cm')->unsigned()->default(149);
+            $table->tinyInteger('age_min_administratif')->unsigned()->default(4);
+            $table->tinyInteger('niveau_num_global')->unsigned():
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reprises');
+    }
+};
