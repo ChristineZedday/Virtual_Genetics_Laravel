1378db9eff055ae244e9b948a68bc3169bfb103f
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-08-10 21:42:15 +0200

    rendements

--- database/seeders/RendementsTableSeeder.php
+++ database/seeders/RendementsTableSeeder.php
@@ -23,28 +23,28 @@ class RendementsTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'mois' => '1',
-                'ha_par_UGB' => 5.0,
+                'ha_par_UGB' => 3.0,
                 'foin_tMS_ha' => 0.0,
             ),
             1 => 
             array (
                 'id' => 2,
                 'mois' => '2',
-                'ha_par_UGB' => 5.0,
+                'ha_par_UGB' => 3.0,
                 'foin_tMS_ha' => 0.0,
             ),
             2 => 
             array (
                 'id' => 3,
                 'mois' => '3',
-                'ha_par_UGB' => 0.45,
+                'ha_par_UGB' => 1.0,
                 'foin_tMS_ha' => 0.0,
             ),
             3 => 
             array (
                 'id' => 4,
                 'mois' => '4',
-                'ha_par_UGB' => 0.3,
+                'ha_par_UGB' => 0.5,
                 'foin_tMS_ha' => 0.0,
             ),
             4 => 
@@ -52,55 +52,55 @@ class RendementsTableSeeder extends Seeder
                 'id' => 5,
                 'mois' => '5',
                 'ha_par_UGB' => 0.25,
-                'foin_tMS_ha' => 3.0,
+                'foin_tMS_ha' => 1.0,
             ),
             5 => 
             array (
                 'id' => 6,
                 'mois' => '6',
-                'ha_par_UGB' => 0.35,
-                'foin_tMS_ha' => 1.5,
+                'ha_par_UGB' => 0.3,
+                'foin_tMS_ha' => 3.0,
             ),
             6 => 
             array (
                 'id' => 7,
                 'mois' => '7',
-                'ha_par_UGB' => 0.45,
+                'ha_par_UGB' => 0.75,
                 'foin_tMS_ha' => 1.0,
             ),
             7 => 
             array (
                 'id' => 8,
                 'mois' => '8',
-                'ha_par_UGB' => 0.6,
-                'foin_tMS_ha' => 0.5,
+                'ha_par_UGB' => 1.0,
+                'foin_tMS_ha' => 0.0,
             ),
             8 => 
             array (
                 'id' => 9,
                 'mois' => '9',
-                'ha_par_UGB' => 0.6,
-                'foin_tMS_ha' => 1.0,
+                'ha_par_UGB' => 0.75,
+                'foin_tMS_ha' => 0.0,
             ),
             9 => 
             array (
                 'id' => 10,
                 'mois' => '10',
-                'ha_par_UGB' => 0.5,
+                'ha_par_UGB' => 0.25,
                 'foin_tMS_ha' => 1.5,
             ),
             10 => 
             array (
                 'id' => 11,
                 'mois' => '11',
-                'ha_par_UGB' => 0.8,
+                'ha_par_UGB' => 0.75,
                 'foin_tMS_ha' => 0.0,
             ),
             11 => 
             array (
                 'id' => 12,
                 'mois' => '12',
-                'ha_par_UGB' => 5.0,
+                'ha_par_UGB' => 2.0,
                 'foin_tMS_ha' => 0.0,
             ),
         ));
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 	<div class='container'>
 		<p>Bienvenue sur mon application simulateur d'élevage de chevaux, basée sur la génétique</p>
 		<p>Pour jouer, vous devez créer au moins un élevage, un budget vous sera alloué pour acheter des reproducteurs et effectuer des actes vétérinaires. Vous pourrez aussi vendre des chevaux.</p>
-		<p>Ceci est la version 1.0 qui ne prend pas en compte le coût d'entretien des chevaux (profitez-en bien, petits veinards!) mais qui ne propose pas non plus de compétitions.</p>
+		<p>Ceci est la version 3.0 qui inclut les compétitions de Modèle et Allures et de Dressage.</p>
 		<p>Vos chevaux et poneys naissent, grandissent et vieillissent en fonction du temps: c'est vous qui choisissez votre rythme, en cliquant sur "avancer d'un mois". Attention: Si vous avez plusieurs élevages, tous seront concernés! Ne changez de mois que lorsque vous avez fait tout ce que vous aviez à faire dans tous vos élevages, et entendez-vous avec les autres joueurs si vous êtes plusieurs!</p>
 	
 		<p>Pour en savoir plus sur la génétique des chevaux, allez sur <a href="http://chrizedday.free.fr">mon site</a>, entre autres!</p>
