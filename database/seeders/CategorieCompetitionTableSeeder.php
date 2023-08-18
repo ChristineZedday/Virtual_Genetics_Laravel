2cd86eb33e7d906a6e5855a34075d5d487c0d7ab
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-08-18 13:03:54 +0200

    seed

--- database/seeders/CategorieCompetitionTableSeeder.php
+++ database/seeders/CategorieCompetitionTableSeeder.php
@@ -3587,6 +3587,14 @@ class CategorieCompetitionTableSeeder extends Seeder
                 'competition_id' => 62,
                 'categorie_id' => 20,
             ),
+            446 => 
+            array (
+                'id' => 493,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'competition_id' => 56,
+                'categorie_id' => 20,
+            ),
         ));
         
         
