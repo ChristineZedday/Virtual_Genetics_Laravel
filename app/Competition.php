83850f7a6b08d9994c0bee8650de9e041294bbef
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-08-27 15:16:22 +0200

    enr comp

--- app/Competition.php
+++ app/Competition.php
@@ -74,7 +74,9 @@ Au moment de l'inscription, les animaux sont inscrits dans un évènement, pour 
 
     public function verification($animal, $evenement, $reprise=NULL)
     {
-        
+        if ($animal->satut_administratif != 'enregistré') {
+            return 'Votre cheval doit être enregistré!';
+        }
         //Vérifie que le cheval est bien incrit dans une compétition correspondant à sa race et à son niveau
         //Possibilité se zapper le niveau départemental 
         $race = $animal->race_id;
