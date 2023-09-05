ceafd6c636d3f33e4cb1e1899e192f9a8950195d
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2023-09-05 20:48:07 +0200

    frais tr

--- app/Elevage.php
+++ app/Elevage.php
@@ -206,7 +206,7 @@ public function acheteTerres($surface)
 public function fraisTransport($animal,$distance) 
 {
     $taille = $animal->taille();
-    $frais = $distance * $taille / 150;
+    $frais = $distance * $taille / 170;
     if (NULL != $animal->StatutFemelle && $animal->StatutFemelle->suitee ) {
         $frais += $frais * 0.5;
     }
