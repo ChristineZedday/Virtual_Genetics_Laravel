e897fa71d5c8ccd964e66200657c041ac2372a8f
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2025-11-26 20:12:27 +0100

    nnb

--- app/Gamedata.php
+++ app/Gamedata.php
@@ -233,20 +233,16 @@ static function checkApprovals () {
         switch (true) {
             case $animal->StatutMale->qualite == 'approuvé an prochain':
                $animal->statutMale->setApproval(); 
-               if ($animal->elevage->role == 'vendeur') {
-                $animal->statutMale->carnet_saillies = true;
-               }
                break;
             case $animal->StatutMale->qualite == 'approbation provisoire an prochain':
                 $animal->statutMale->setProvisoire(); 
-                 if ($animal->elevage->role == 'vendeur') {
-                $animal->statutMale->carnet_saillies = true;
-               }
+             
                break;
             case $animal->StatutMale->qualite == 'approbation provisoire cette année':
                 $animal->statutMale->setModele15(false); 
                 $animal->statutMale->setClasseNat(false);
                 $animal->statutMale->setApproval(false);
+               
                    //il doit se requalifier
                break;
                
